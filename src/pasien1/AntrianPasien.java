dd8bce2dc304030d00e2823cb391551231f45453
jarkom <jarkom@jarkom-C00>
2018-08-31 12:54:11 +0700

    Versi 0.1

---
+++ src/pasien1/AntrianPasien.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasien1;
+
+/**
+ *
+ * @author jarkom
+ */
+public class AntrianPasien {
+
+    private int tanggalAntrian;
+    private int bulanAntrian;
+    private int tahunAntrian;
+    private int nomorAntrian = 0;
+    private final int JUMLAH_MAKSIMAL_PASIEN = 50;
+    private Pasien[] daftarPasien = new Pasien[JUMLAH_MAKSIMAL_PASIEN];
+/**
+ * 
+ * @param pasien
+ * @throws Exception 
+ */
+    public void mendaftar(Pasien pasien) throws Exception {
+        if (nomorAntrian < JUMLAH_MAKSIMAL_PASIEN) {
+            daftarPasien[nomorAntrian] = pasien;
+            nomorAntrian++;
+        } else {
+            throw new Exception("antrian penuh");
+
+        }
+        /**
+         * 
+         */
+    }public Pasien panggilPasien(int nomorAntrian){
+            if (this.nomorAntrian < JUMLAH_MAKSIMAL_PASIEN) {
+                
+            }
+            return daftarPasien[nomorAntrian];
+            
+    }
+}
